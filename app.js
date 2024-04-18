3a0ee42285c34d2315da3f40fb6793acf1abc011
JayFed <167407013+JayFed@users.noreply.github.com>
2024-04-18 22:02:49 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+let Donate = document.querySelector(".button-37")
+let img = document.querySelector(".Donate-content")
+let x = 0
+
+function donate(){
+    x++
+    if(x%2 != 0){
+        Donate.style.height="26rem"
+        Donate.style.width="350px"
+        img.style.display="block"
+        
+    }else{
+        img.style.display="none"
+        Donate.style.height="2.5rem"
+        Donate.style.width="200px"
+    }
+}
